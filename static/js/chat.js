98b1bcee7686e474ec7edf4f6d2129239cbbcf02
Kári Tristan Helgason <kthelgason@gmail.com>
2015-09-19 23:37:55 +0000

    Adds client side logic for sending messages

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,16 +1,36 @@
 
 function bindEventSource () {
-    var es = new EventSource('/stream');
-    var chatMessages = document.getElementById("chat").children[1];
-    es.onmessage = function (evt) {
-      chatMessages.innerHTML += "<tr><td>" + evt.data + "</td></tr>";
-    };
-    es.onerror = function (err) {
-      console.error("Error occured:", err);
-    };
+  var es = new EventSource('/stream');
+  var chatMessages = document.getElementById("chat").children[1];
+  es.onmessage = function (evt) {
+    chatMessages.innerHTML += "<tr><td>" + evt.data + "</td></tr>";
+  };
+  es.onerror = function (err) {
+    console.error("Error occured:", err);
+  };
+}
+
+function postMessage (message) {
+  var req = new XMLHttpRequest();
+  req.open("POST", "/message");
+  req.onload = function () {
+    console.log("posted.");
+  };
+  req.send(message);
+}
+
+function bindSendEvents () {
+  var form = document.getElementById("post");
+  var messageInput = form.children[0].children[0];
+  form.addEventListener("submit", function (e) {
+    postMessage(messageInput.value);
+    messageInput.value = "";
+    e.preventDefault();
+  });
 }
 
 
 window.onload = function () {
   bindEventSource();
+  bindSendEvents();
 };
