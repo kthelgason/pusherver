1b49f718cfda5a81119a2f3d3813c789deb0ab9f
Kári Tristan Helgason <kthelgason@gmail.com>
2015-09-19 20:55:18 +0000

    Adds sse stream endpoint

--- server.rb
+++ server.rb
@@ -2,18 +2,28 @@
 require 'sinatra'
 require 'json'
 
-set server: 'thin'
+set server: 'thin', connections: []
 
 set :public_folder, File.dirname(__FILE__) + '/static'
 
 messages = []
+
 
 get '/' do
   erb :index, :locals => { messages: messages }
 end
 
 post '/message' do
-  messages << JSON.parse(request.body.read)
+  settings.connections.each do |sock|
+    sock << JSON.parse(request.body.read)
+  end
   201
+end
+
+get '/stream', provides: 'text/event-stream' do
+  stream :keep_open do |sock|
+    settings.connections << sock
+    sock.callback { settings.connections.delete sock }
+  end
 end
 
