59a2209c0862c2da18701b6769cf7541f9e8bd01
Kári Tristan Helgason <kthelgason@gmail.com>
2015-09-19 12:11:22 +0000

    Adds message post endpoint

--- server.rb
+++ server.rb
@@ -1,7 +1,16 @@
 require 'sinatra'
+require 'json'
 
 set :public_folder, File.dirname(__FILE__) + '/static'
 
+messages = []
+
 get '/' do
-  erb :index
+  erb :index, :locals => { messages: messages }
 end
+
+post '/message' do
+  messages << JSON.parse(request.body.read)
+  201
+end
+
