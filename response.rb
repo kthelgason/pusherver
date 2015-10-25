8665faf15206d5fd32bdd94608bf1a78e098cf3d
Kári Tristan Helgason <kthelgason@gmail.com>
2015-10-25 17:26:29 +0000

    Refactor response stuff into own class

---
+++ response.rb
@@ -0,0 +1,58 @@
+# coding: utf-8
+require 'rubygems'
+require 'erb'
+
+class HttpResponse
+  attr_accessor :status, :status_string, :content, :headers
+
+  STATICDIR = "static"
+  CONTENT_TYPES = {
+    "js" => "application/javascript",
+    "html" => "text/html",
+    "css" => "text/css"
+  }
+
+  def initialize
+    @headers = {
+    "Server" => "Pusherver 0.0.2",
+    "Connection" => "close"
+    }
+    @content = ""
+  end
+
+  def render_template(tmpl, mapping)
+    b = binding
+    mapping.each do |k,v|
+      b.local_variable_set(k, v)
+    end
+    File.open tmpl, "r" do |f|
+      @content = ERB.new(f.read()).result(b)
+    end
+    @headers["Content-type"] = "text/html"
+  end
+  
+  def serve_static(filename)
+    ext = filename.split('.').last
+    File.open filename, "r" do |f|
+      @content = f.read
+    end
+    @headers["Content-type"] = CONTENT_TYPES.fetch(ext, "text/plain")
+  end
+
+  def not_found
+    @status = 404
+    @status_string = "Not found"
+  end
+
+  def to_s
+    string =  "HTTP/1.1 #{@status || 200} #{@status_string || "OK"}\r\n"
+    @headers.each do |k,v|
+      string << "#{k}: #{v}\r\n"
+    end
+    string << "Content-length: #{@content.bytesize + 2}\r\n"
+    string << "\r\n"
+    string << @content
+    string << "\r\n"
+  end
+
+end
--- server.rb
+++ server.rb
@@ -2,61 +2,39 @@
 require 'rubygems'
 require 'date'
 require 'eventmachine'
-require 'erb'
+require './response'
 
-CRLF = "\r\n"
 PORT = 4444
 STATICDIR = "static"
 
-def hello
-  render_template "views/index.erb", {messages: []}
+def hello(response)
+  response.render_template "views/index.erb", {messages: []}
 end
 
 request_handlers = {
   "/" => method(:hello)
 }
 
-class StaticFile
-  CONTENT_TYPES = {
-    "js" => "application/javascript",
-    "html" => "text/html",
-    "css" => "text/css"
-  }
-  def initialize(filename)
-    @filename = filename
-    @extention = filename.split('.').last
-  end
-
-  def read
-    File.open @filename, "r" do |f|
-      f.read
-    end
-  end
-
-  def content_type
-    CONTENT_TYPES.fetch(@extention, "text/plain")
-  end
-end
-
-
 class SimpleHandler < EM::P::HeaderAndContentProtocol
   attr_accessor :handlers
+
   def receive_request(headers, content)
     @headers = headers_2_hash headers
     parse_req_line headers.first
     puts "#{DateTime.now}\t#{@method} #{@uri}"
+    response = HttpResponse.new
     begin
-      content, content_type = @handlers.fetch(@uri).call
-      send_response(200, "OK", content, content_type: content_type)
+      @handlers.fetch(@uri).call(response)
+      send_response(response)
     rescue KeyError
       # No handler, try serving from static dir
       full_path = STATICDIR + @uri
       if File.exist? full_path
-        file = StaticFile.new full_path
-        send_response(200, "OK", file.read, content_type: file.content_type)
+        response.serve_static(full_path)
+        send_response(response)
       else
         # Resource not found
-        send_response(404, "Not Found")
+        send_response(response.not_found)
       end
     end
   end
@@ -67,27 +45,10 @@ class SimpleHandler < EM::P::HeaderAndContentProtocol
     @uri, @query = uri.split('?')
   end
 
-  def send_response(code, text, response="", content_type: "text/plain")
-    send_data "HTTP/1.1 #{code} #{text}#{CRLF}" +
-              "Server: Pusherver 0.0.2#{CRLF}" +
-              "Content-type: #{content_type}#{CRLF}" +
-              "Connection: close#{CRLF}" +
-              "Content-length: #{response.bytesize + 2}#{CRLF}" +
-              CRLF + response + CRLF
+  def send_response(res)
+    send_data res
     close_connection_after_writing
   end
-end
-
-def render_template(tmpl, mapping)
-  page = ""
-  b = binding
-  mapping.each do |k,v|
-    b.local_variable_set(k, v)
-  end
-  File.open tmpl, "r" do |f|
-    page = ERB.new(f.read()).result(b)
-  end
-  return page, "text/html"
 end
 
 EventMachine.run do
